6b2efeea051775b76a3547e463d1d22f37dcfc21
b112137 <e0010209@gmail.com>
2021-03-24 20:17:25 +0800

    ser.js

--- HW2/ser.js
+++ HW2/ser.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
-
-// Step 1 and step 2 code goes here
-
 // include `express`
 const express = require('express');
 const fs = require('fs');
@@ -11,47 +8,16 @@ const app = express()
 
 const port = 26969
 
-// handle `/step1` url
-app.get('/step1', (req, res) => {
-   // response browser
-  res.send('hello world')
-})
-
 // start the server
 app.listen(port, () => {
   console.log(`listening on port: ${port}`)
 })
 
-app.get('/step2', (req, res) => {
-  res.send('<h1>hello world</h1>');
-})
-
-// Step 3 code goes here
-
 app.use(express.static(`${__dirname}/dist`))
-
-
-// Step 4 code goes here
-
-let nRequests = 0
-app.get('/step4', (req, res) => {
-    res.send(`this is request #${++nRequests}`)
-}) 
-
-// Step 5
-
-app.get('/step5', (req, res) => {
-    res.send(`Hello, ${req.query.fname} ${req.query.lname}`)
-})
-
-
-
-
 
 app.get('/list', (req, res) => {
   var file = "students.sample.json";
   var result = JSON.parse(fs.readFileSync(file));
-  // var result_string = JSON.stringify(result);
   console.log(result);
   res.send(result);
 })
